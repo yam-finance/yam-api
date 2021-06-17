b43c8ab3044de4092d308c5186e784e4c56cb17d
byterose <>
2021-06-17 23:57:36 +0200

    Update spreadsheet call

--- source/routes/asset-index.go
+++ source/routes/asset-index.go
@@ -2,9 +2,7 @@ package routes
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -15,120 +13,51 @@ import (
 
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/go-chi/chi"
-	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
-	"google.golang.org/api/option"
-	"google.golang.org/api/sheets/v4"
+	"gopkg.in/Iwark/spreadsheet.v2"
 )
 
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return config.Client(context.Background(), tok)
-}
-
-// Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
-}
-
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
-
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-}
-
-func FetchAssetIndex() map[string]interface{} {
+func FetchAssetIndex(sheetTitle string, cycleName string) map[string]interface{} {
 	eval_ts := int(time.Now().UTC().Unix())
-	ctx := context.Background()
 
-	clientSecret := os.Getenv("CLIENT_SECRET")
-	b := fmt.Sprintf(
-	`{
-	    "installed": {
-	      "client_id": "697263365567-06hbtfo7cksb54l052n9mv0l053a8pmu.apps.googleusercontent.com",
-	      "project_id": "quickstart-1615241292139",
-	      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-	      "token_uri": "https://oauth2.googleapis.com/token",
-	      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-	      "client_secret": "%s",
-	      "redirect_uris": [
-	        "urn:ietf:wg:oauth:2.0:oob",
-	        "http://localhost"
-	      ]
-	    }
-	  }`, clientSecret)
+	privateKeyId := os.Getenv("PRIVATE_KEY_ID")
+	privateKey := os.Getenv("PRIVATE_KEY")
+	clientId := os.Getenv("CLIENT_ID")
+	data := fmt.Sprintf(`{
+		"type": "service_account",
+		"project_id": "index-ustonks",
+		"private_key_id": "%s",
+		"private_key": "%s",
+		"client_email": "yam-api@index-ustonks.iam.gserviceaccount.com",
+		"client_id": "%s",
+		"auth_uri": "https://accounts.google.com/o/oauth2/auth",
+		"token_uri": "https://oauth2.googleapis.com/token",
+		"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+		"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/yam-api%%40index-ustonks.iam.gserviceaccount.com"
+	}`, privateKeyId, privateKey, clientId)
 
-	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON([]byte(b), "https://www.googleapis.com/auth/spreadsheets.readonly")
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-	}
-	client := getClient(config)
+	conf, err := google.JWTConfigFromJSON([]byte(data), spreadsheet.Scope)
+	checkError(err)
+	client := conf.Client(context.TODO())
 
-	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
-	if err != nil {
-		log.Fatalf("Unable to retrieve Sheets client: %v", err)
-	}
-
+	service := spreadsheet.NewServiceWithClient(client)
 	spreadsheetId := os.Getenv("SHEET_ID")
-	readRange := "M50"
-	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve data from sheet: %v", err)
-	}
+	spreadsheet, err := service.FetchSpreadsheet(spreadsheetId)
+	checkError(err)
+	sheet, err := spreadsheet.SheetByTitle(sheetTitle)
+	checkError(err)
 
-	var price interface{}
+	price := sheet.Columns[12][49].Value
 
-	if len(resp.Values) == 0 {
-		log.Fatalf("No data found.")
-	} else {
-		price = resp.Values[0][0]
-	}
-
-	fmt.Println(price)
-
-	values := map[string]interface{}{"cycle": "USTONKS-0121", "price": price, "timestamp": strconv.Itoa(eval_ts)}
+	values := map[string]interface{}{"cycle": cycleName, "price": price, "timestamp": strconv.Itoa(eval_ts)}
 
 	return values
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
 // --------- uSTONKS Endpoints ---------
@@ -150,8 +79,7 @@ func GetLatestUStonksIndex(path string, router chi.Router, conf *config.Config, 
 		)
 
 		if values == nil {
-			values = FetchAssetIndex()
-			mongodb.InsertPunkIndex(values)
+			fmt.Errorf("No db entry for uSTONKS index.")
 		}
 	})
 }
@@ -175,8 +103,7 @@ func GetLatestUStonksIndexHistory(path string, router chi.Router, conf *config.C
 		)
 
 		if history == nil {
-			values := FetchAssetIndex()
-			mongodb.InsertPunkIndex(values)
+			fmt.Errorf("No db entry for uSTONKS index.")
 		}
 	})
 }
