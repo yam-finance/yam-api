9de3bd51701911a30ea78eb2df7f04990f578904
ethedev <2432272+ethedev@users.noreply.github.com>
2021-06-23 11:04:40 +0400

    Fix apr commenting

--- source/routes/index.go
+++ source/routes/index.go
@@ -36,9 +36,9 @@ func Initialize(conf *config.Config, geth *ethclient.Client) chi.Router {
 
 	// YAM
 	Treasury("/treasury", router, conf, geth)
-	/*Apr("/apr", router, conf, geth)
-	AprYam("/apr/yam", router, conf, geth)*/
-	AprDegenerative("/apr/degenerative", router, conf, geth)
+	Apr("/apr", router, conf, geth)
+	AprYam("/apr/yam", router, conf, geth)
+	/*AprDegenerative("/apr/degenerative", router, conf, geth)*/
 	Tvl("/tvl", router, conf, geth)
 	TvlYam("/tvl/yam", router, conf, geth)
 	TvlDegenerative("/tvl/degenerative", router, conf, geth)
