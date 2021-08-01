67963d30dbb482e256b3bd80515208fafc2a6c11
byterose <>
2021-08-01 15:52:01 +0200

    Update assets.json link

--- source/routes/assets.go
+++ source/routes/assets.go
@@ -15,7 +15,7 @@ import (
 func GetAssetsJson(path string, router chi.Router, conf *config.Config, geth *ethclient.Client) {
 	router.Get(path, func(w http.ResponseWriter, r *http.Request) {
 
-		resp, err := http.Get("https://raw.githubusercontent.com/yam-finance/degenerative-sdk/master/src/assets.json")
+		resp, err := http.Get("https://raw.githubusercontent.com/yam-finance/synths-sdk/master/src/assets.json")
 		if err != nil {
 			log.Fatalln(err)
 		}
